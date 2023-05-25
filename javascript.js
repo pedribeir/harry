51222a332cbdef6d2519a9e6e95a9d58a8b7ad4d
pedroca <131361959+pedribeir@users.noreply.github.com>
2023-05-25 20:42:10 -0300

    de novo
    eu não aguento maaaaaaaaaaaaaaaaaaais

---
+++ javascript.js
@@ -0,0 +1,15 @@
+// Show the first tab and hide the rest
+$('#tabs-nav li:first-child').addClass('active');
+$('.tab-content').hide();
+$('.tab-content:first').show();
+
+// Click function
+$('#tabs-nav li').mouseenter(function(){
+  $('#tabs-nav li').removeClass('active');
+  $(this).addClass('active');
+  $('.tab-content').hide();
+  
+  var activeTab = $(this).find('a').attr('href');
+  $(activeTab).fadeIn();
+  return false;
+});
