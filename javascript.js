3c378708cbb03796473ec6ae07bda7cea97fc14c
pedribeiro <ribeiroaugus@gmail.com>
2023-07-03 21:07:55 -0300

    color

---
+++ javascript.js
@@ -0,0 +1,7 @@
+<script>
+let menuToggle = document.querySelector('.menuToggle')
+let header = document.querySelector('header')
+menuToggle.onclick = function(){
+    header.classList.toggle('active')
+}
+</script>
